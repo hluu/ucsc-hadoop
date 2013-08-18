37e528fcb496936c85ea06e9eda1e8e09ac9979b
Hien Luu <hluu@yahoo.com>
2013-08-18 11:42:14 -0700

    Fixed MovieAvroCount

--- src/main/java/ucsc/hadoop/mapreduce/avro/MovieAvroCount.java
+++ src/main/java/ucsc/hadoop/mapreduce/avro/MovieAvroCount.java
@@ -2,14 +2,19 @@ package ucsc.hadoop.mapreduce.avro;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
 
 import org.apache.avro.Schema;
+import org.apache.avro.Schema.Field;
+import org.apache.avro.Schema.Type;
 import org.apache.avro.file.DataFileStream;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericDatumReader;
 import org.apache.avro.mapred.AvroKey;
 import org.apache.avro.mapreduce.AvroJob;
 import org.apache.avro.mapreduce.AvroKeyInputFormat;
+import org.apache.avro.mapreduce.AvroKeyOutputFormat;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configured;
@@ -28,6 +33,8 @@ import org.apache.hadoop.util.ToolRunner;
 public class MovieAvroCount extends Configured implements Tool {
 
 	private static final Log LOG = LogFactory.getLog(MovieAvroCount.class);
+	
+	private static Schema OUTPUT_SCHEMA;
 
 	/**
 	 * @param args
@@ -40,7 +47,7 @@ public class MovieAvroCount extends Configured implements Tool {
 	@Override
 	public int run(String[] args) throws Exception {
 		if (args.length != 2) {
-			System.err.println("Usage: avromoviecount <in> <out>");
+			System.err.println("Usage: avromoviecount [-D outputFormat=avro] <in> <out>");
 			System.exit(2);
 		}
 
@@ -59,10 +66,30 @@ public class MovieAvroCount extends Configured implements Tool {
 		job.setMapperClass(MovieTokenizerMapper.class);
 		job.setMapOutputKeyClass(IntWritable.class);
 		job.setMapOutputValueClass(IntWritable.class);
-		
+
 		job.setReducerClass(MovieCountReducer.class);
 
 		AvroJob.setInputKeySchema(job, movieSchema);
+
+		String outputFormat = getConf().get("outputFormat", "text");
+		LOG.info("---- outputFormat: " + outputFormat);
+		if ("avro".equalsIgnoreCase(outputFormat)) {
+			
+			LOG.info("---- create OUTPUT_SCHEMA ---");
+			
+			OUTPUT_SCHEMA = Schema.createRecord("movieCount", null, null, false);
+					
+			List<Field> fieldList = Arrays.asList(
+					new Schema.Field("year", Schema.create(Type.INT), null, null), 
+					new Schema.Field("count", Schema.create(Type.INT), null, null));
+			OUTPUT_SCHEMA.setFields(fieldList);
+			
+			AvroJob.setOutputKeySchema(job, OUTPUT_SCHEMA);
+		    job.setOutputFormatClass(AvroKeyOutputFormat.class);
+			job.setReducerClass(MovieCountAvroReducer.class);
+		}
+		
+		LOG.info("----  reduce class: " + job.getReducerClass().getName());
 
 		job.setInputFormatClass(AvroKeyInputFormat.class);
 		boolean result = job.waitForCompletion(true);
@@ -118,14 +145,37 @@ public class MovieAvroCount extends Configured implements Tool {
 			context.write(key, SUM);
 
 		}
+	}
 
-		/*
-		 * public void reduce(Integer year, Iterable<Integger> values,
-		 * AvroCollector<Pair<Integer, Integer>> collector, Reporter reporter)
-		 * throws IOException {
-		 * 
-		 * collector.collect(new Pair<Integer, Integer>(year, ONE)); }
-		 */
+	/**
+	 * This reducer writes out the data in Avro generic record
+	 * 
+	 * @author hluu
+	 *
+	 */
+	public static class MovieCountAvroReducer extends
+			Reducer<IntWritable, IntWritable, AvroKey<GenericData.Record>, NullWritable> {
+
+		@Override
+		protected void setup(Context context) throws IOException,
+				InterruptedException {
+		}
+
+		@Override
+		public void reduce(IntWritable key, Iterable<IntWritable> values,
+				Context context) throws IOException, InterruptedException {
+
+			int sum = 0;
+			for (IntWritable value : values) {
+				sum += value.get();
+			}
+
+			GenericData.Record outputRecord = new GenericData.Record(OUTPUT_SCHEMA);
+			outputRecord.put("year", new Integer(key.get()));
+			outputRecord.put("count", new Integer(sum));
+			context.write(new AvroKey<GenericData.Record>(outputRecord), NullWritable.get());
+			
+		}
 	}
 
 }
